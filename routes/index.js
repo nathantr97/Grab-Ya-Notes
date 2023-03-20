301f6e604eb0a904558834ba1dec896459ced77f
Nathan <119708133+nathantr97@users.noreply.github.com>
2023-03-20 11:34:15 -0700

    finalized code

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the feedback
+notes.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+// POST Route for submitting feedback
+notes.post('/', (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if ( title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting feedback');
+  }
+});
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const app = express();
 const PORT = process.env.port || 3001;
 const api = require("./routes/index");
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use("/api/notes", api);
 
-app.use("/api", api);
+app.use(express.static("public"));
 
 app.get("/notes", (req, res)=> {
-    res.sendFile(__dirname + "/public/notes.html", function (error) {
+    res.sendFile(__dirname + "/public/notes.html"), function (error) {
         if (error) {
             res.status(500).send(error);
         }
-    })
+    }
 });
 
 app.get("*", (req, res) => {
