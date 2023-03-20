f181d2ecec6ae32e70d5abddd7966e6f289f93cb
Nathan <119708133+nathantr97@users.noreply.github.com>
2023-03-20 13:53:01 -0700

    port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 const api = require("./routes/index");
 
 app.use(express.json());
